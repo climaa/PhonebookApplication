6155836fe7e0017f51958b046143182c2143c746
Carlos Lima <carlos.lima@distillery.com>
2024-02-27 11:22:23 -0600

    Added apple icons

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Image} from 'react-native';
 
 import {ManageContactSection, ContactsList} from './app/sections';
 
@@ -16,9 +17,41 @@ const Tab = createBottomTabNavigator();
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name="Contact list" component={ContactsList} />
-        <Tab.Screen name="New contact" component={ManageContactSection} />
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          headerShown: false,
+          tabBarIcon: ({size}) => {
+            if (route.name === 'Contact list') {
+              return (
+                <Image
+                  source={require('./app/images/icons8-directory-80.png')}
+                  style={{width: size, height: size}}
+                />
+              );
+            }
+
+            if (route.name === 'New contact') {
+              return (
+                <Image
+                  source={require('./app/images/icons8-new-contact-80.png')}
+                  style={{width: size, height: size}}
+                />
+              );
+            }
+
+            return null;
+          },
+        })}>
+        <Tab.Screen
+          component={ContactsList}
+          name="Contact list"
+          options={{title: 'Contact list'}}
+        />
+        <Tab.Screen
+          component={ManageContactSection}
+          name="New contact"
+          options={{title: 'Add contact'}}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
